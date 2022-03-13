2df79bfae4750c11ab7281e93644d3e159ea26f0
kanishk <kanishk.9871@gmail.com>
2022-03-13 20:30:20 +0530

    Aded four pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 import {
-	BrowserRouter as Router,
-	Navigate,
-	Route,
-	Routes,
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
 } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Blog from "./components/Blog/Blog";
 import Contact from "./components/Contact/Contact";
+import Agents from "./components/Agents/Agents";
+import Properties from "./components/Properties/Properties";
+import SingleProperty from "./components/SingleProperty/SingleProperty";
+import SingleAgent from "./components/SingleAgent/SingleAgent";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route exact path={"/"} element={<Home />} />
-				<Route exact path={"/about"} element={<About />} />
-				<Route exact path={"/blog"} element={<Blog />} />
-                				<Route exact path={"/contactus"} element={<Contact />} />
+  return (
+    <Router>
+      <Routes>
+        <Route exact path={"/"} element={<Home />} />
+        <Route exact path={"/about"} element={<About />} />
+        <Route exact path={"/blog"} element={<Blog />} />
+        <Route exact path={"/contactus"} element={<Contact />} />
+        <Route exact path={"/agents"} element={<Agents />} />
+        <Route exact path={"/properties"} element={<Properties />} />
+        <Route exact path={"/single-property"} element={<SingleProperty />} />
+        <Route exact path={"/single-agent"} element={<SingleAgent />} />
 
-			</Routes>
-		</Router>
-	);
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
